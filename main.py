f03c615245244a5c28c0c0eccae96a3f40723c67
WilliamSpanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-04-07 18:25:52 +0300

    Clean up get_item_details_for(item_url) method...
    ..., with item_price variable and use string strip method to remove extraneous white space from title string.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
 
 CHAIR_URL = "https://www.amazon.com/BZLSFHZ-Ergonomic-Adjustable-Headrest-Breathable/dp/B09MB1QT4G/ref=sr_1_206?crid=1BTMETGBD2VVO&keywords=chairs+for+desk&qid=1649319961&sprefix=chairs%2Caps%2C394&sr=8-206"
 BAND_URL = "https://www.amazon.com/TalkWorks-Compatible-Stainless-Adjustable-Magnetic/dp/B0866BKBK8/ref=pd_pb_ss_no_hpb_sccl_1/140-0619034-7057416?pd_rd_w=ZzDY0&pf_rd_p=45f92aae-3fbe-4e26-9929-951264041217&pf_rd_r=N89FZ7Q4EV7PS7N6RMK5&pd_rd_r=9a635269-9908-4b39-996f-7d789898eb18&pd_rd_wg=vE6AZ&pd_rd_i=B0866BKBK8&psc=1"
@@ -19,10 +20,12 @@ def get_item_details_for(item_url):
     soup = BeautifulSoup(response.content, "lxml")
     item_price_no_currency = soup.find("span", "a-offscreen").getText().split("$")[1]
     item_price_no_commas = item_price_no_currency.replace(",", "")
+    item_price = float(item_price_no_commas)
 
-    item_title = soup.find("div", id="titleSection").span.getText()
+    # Remove white-space from item_title string with strip() method
+    item_title = soup.find("div", id="titleSection").span.getText().strip()
     # print(f"title is: {item_title}")
-    return (item_title, float(item_price_no_commas), item_url)
+    return (item_title, item_price, item_url)
 
 
 item_details = get_item_details_for(CHAIR_URL)
